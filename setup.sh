ebf9b37182268ecca482f40f68633970e7812192
Kyle Derby MacInnis <director@sovereign.enterprises>
2021-11-19 20:44:38 -0700

    adding setup

---
+++ setup.sh
@@ -0,0 +1,4 @@
+npm install webpack webpack-cli webpack-dev-server react-dom --save-dev
+npm install @babel/cli @babel/core @babel/node @babel/preset-env @babel/preset-react @babel/register babel-register --save-dev
+npm install babel-loader style-loader sass-loader --legacy-peer-deps
+npx webpack
